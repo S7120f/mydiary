0c534caa1619eaa473f2ab8a818a7d6bbf3546af
Your Namejonas <skonby_jonas@hotmail.com>
2024-12-19 19:33:03 +0100

    created the function in new-post so user are able to crate a new post with date, title, content, and this will show in forum all-posts

--- src/main/java/com/mydiary/mydiary/CreatePostController.java
+++ src/main/java/com/mydiary/mydiary/CreatePostController.java
@@ -1,16 +1,42 @@
 package com.mydiary.mydiary;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class CreatePostController {
     
+    @Autowired
+    private DiaryRepository diaryRepository;
 
     @GetMapping("/new-post")
-    public String newPost(){
+    public String newPost(Model model){
+
+        // model.addAttribute("allPosts", diaryRepository.findAll());
 
         return"new-post";
+    }
+
+    @PostMapping("create-post")
+    public String addNewPost(@RequestParam("title") String titleName, @RequestParam("content") String contentName, @RequestParam("dateNow") LocalDate dateName ){
+
+
+        System.out.println("Nytt inlägg från forumet " + titleName + " " + dateName + " " + contentName);
+        Diary diary = new Diary();
+        diary.setTitle(titleName);
+        diary.setContent(contentName);
+        diary.setDate(dateName);
+        diaryRepository.save(diary);
+
+
+        return "redirect:/new-post";
     }
 
 }
--- src/main/java/com/mydiary/mydiary/PostsController.java
+++ src/main/java/com/mydiary/mydiary/PostsController.java
@@ -1,15 +1,23 @@
 package com.mydiary.mydiary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostsController {
-    
+
+    @Autowired
+    private DiaryRepository diaryRepository;
 
     @GetMapping("/all-posts")
-    public String allPosts(){
+    public String allPosts(Model model){
+
+        model.addAttribute("allPosts", diaryRepository.findAll());
+
 
         return "all-posts";
     }
+
 }
