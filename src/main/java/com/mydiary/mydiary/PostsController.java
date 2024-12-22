0131a3d168880febc50cb0a587f2270c5f443866
Your Namejonas <skonby_jonas@hotmail.com>
2024-12-22 16:37:51 +0100

    Function for edit post and updated css

--- src/main/java/com/mydiary/mydiary/PostsController.java
+++ src/main/java/com/mydiary/mydiary/PostsController.java
@@ -1,9 +1,13 @@
 package com.mydiary.mydiary;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostsController {
@@ -12,12 +16,30 @@ public class PostsController {
     private DiaryRepository diaryRepository;
 
     @GetMapping("/all-posts")
-    public String allPosts(Model model){
+    public String allPosts(Model model) {
 
         model.addAttribute("allPosts", diaryRepository.findAll());
 
-
         return "all-posts";
+    }
+
+    @GetMapping("/update-post")
+    public String updatePost(Model model, @RequestParam int id) {
+        Diary diary = diaryRepository.findById(id).orElse(null);
+        if (diary != null) {
+            model.addAttribute("diary", diary);
+            return "post-update";
+
+        }
+
+        return "redirect:/all-posts";
+
+    }
+
+    @PostMapping("/update-post")
+    public String saveUpdatedPost(Diary diary) {
+        diaryRepository.save(diary);
+        return "redirect:/all-posts";
     }
 
 }
